d464165748c3550a823902281f29a69f91b57380
keerthi <keerthi1822@gmail.com>
2023-02-17 23:03:42 +0100

    portfolio

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,5 +1,5 @@
 import React,{ useState } from 'react';
-import { Link } from 'react-router-dom';
+
 /* import LandingPage from '../../LandingPage';
 import AboutMe from '../aboutme/AboutMe';
 import Education from '../education/Education';
