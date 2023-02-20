b9a299bc60486adcf415603bd2c5fda98a0b6ee6
keerthi <keerthi1822@gmail.com>
2023-02-20 12:10:53 +0100

    css fine changes.

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,45 +1,53 @@
-import React,{ useState } from 'react';
-import { Link } from 'react-router-dom';
-/* import LandingPage from '../../LandingPage';
-import AboutMe from '../aboutme/AboutMe';
-import Education from '../education/Education';
-import Portfolio from '../portfolio/Portfolio';
-import Contact from '../contact/Contact'; */
+import React, { useState } from 'react';
+import { Link } from "react-scroll";
 
+/* import { Link } from 'react-router-dom'; */
 
 import './navigation.css'
 
 const Navigation = () => {
 
-   const [active,setActive] = useState()
+  const [active, setActive] = useState()
+  /*   const [isActive, setIsActive] = useState(true && active) */
 
   return (
     <div className='navigation'>
 
       <div className='home-link'>
-        <a href="home"    onClick={() => setActive("home")} className={`${active === "home" ? "active" : ""}`}>
-          H!!OM!!E
-        </a>
+        
+      <Link activeClass="active" smooth spy to="home">
+      H!!OM!!E
+      </Link>
+       
       </div>
-
 
       <ul className='navigation-list'>
 
+        <li>
+          <Link activeClass="active" smooth spy to="AboutMe">
+            About me
+          </Link>
+        </li>
 
-        <li><a href="#AboutMe"   onClick={() => setActive("AboutMe")} className={`${active === "AboutMe" ? "active" : ""}`}>About me </a ></li>
+        <li>
+          <Link activeClass="active" smooth spy to="Portfolio">
+          Portfolio
+          </Link>
+        </li>
 
         <li>
-          <a href="#Portfolio"  onClick={() => setActive("Portfolio")} className={`${active === "Portfolio" ? "active" : ""}`}>
-            Porfolio</a></li>
+        <Link activeClass="active" smooth spy to="Education">
+        Education
+          </Link>
+         </li>
         <li>
-          <a href="#Education"   onClick={() => setActive("Education")} className={`${active === "Education" ? "active" : ""}`}>
-            Education</a></li>
-        <li>
-          <a href="#Contact"  onClick={() => setActive("Contact")} className={`${active === "Contact" ? "active" : ""}`}>
-            Contact</a></li>
-        
+
+        <Link activeClass="active" smooth spy to="EducContactation">
+        Contact
+          </Link>
+         </li>
+
       </ul>
-
     </div>
   )
 }
